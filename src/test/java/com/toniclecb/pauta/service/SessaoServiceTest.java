1cf200b7c4ccec8a3f56e7775ea948f457723882
Cleiton <toniclecb@gmail.com>
2023-06-04 18:26:36 -0300

    Testes da classse SessaoService

--- src/test/java/com/toniclecb/pauta/service/SessaoServiceTest.java
+++ src/test/java/com/toniclecb/pauta/service/SessaoServiceTest.java
@@ -7,40 +7,55 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.toniclecb.pauta.exception.ConflictException;
+import com.toniclecb.pauta.exception.ForbiddenException;
+import com.toniclecb.pauta.exception.ResourceNotFoundException;
+import com.toniclecb.pauta.model.Associado;
 import com.toniclecb.pauta.model.Pauta;
 import com.toniclecb.pauta.model.Sessao;
+import com.toniclecb.pauta.model.Voto;
 import com.toniclecb.pauta.model.dto.SessaoRequestDTO;
 import com.toniclecb.pauta.model.dto.SessaoResponseDTO;
+import com.toniclecb.pauta.model.dto.VotoRequestDTO;
+import com.toniclecb.pauta.model.dto.VotoResponseDTO;
+import com.toniclecb.pauta.repository.AssociadoRepository;
 import com.toniclecb.pauta.repository.PautaRepository;
 import com.toniclecb.pauta.repository.SessaoRepository;
+import com.toniclecb.pauta.repository.VotoRepository;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class SessaoServiceTest {
 
 	@Mock
+    private AssociadoRepository associadoRepository;
+	
+	@Mock
     private SessaoRepository sessaoRepository;
 
 	@Mock
     private PautaRepository pautaRepository;
 	
+	@Mock
+	private VotoRepository votoRepository;
+	
     @InjectMocks
     private SessaoService service;
-        
+
     @Test
     public void testInicioSessao(){
         Pauta pautaEntity = new Pauta(10L, "pauta", "descricao da pauta");
         Sessao sessao = new Sessao(pautaEntity);
-        sessao.setFimVotacao(new Date(1L));
-        sessao.setInicioVotacao(new Date(2L));
         
         Mockito.when(pautaRepository.findById(10L)).thenReturn(Optional.of(pautaEntity));
         Mockito.when(sessaoRepository.save(any())).thenReturn(sessao);
@@ -49,5 +64,94 @@ public class SessaoServiceTest {
         		new SessaoRequestDTO(10L, 3600));
 
         Assert.assertEquals(sessao.getPauta().getId(), response.getIdPauta());
+    }
+
+    @Test
+    public void testVoto(){
+    	Associado associado = new Associado(2L, "Fulano", "36599334067");
+    	Pauta pautaEntity = new Pauta(10L, "pauta", "descricao da pauta");
+    	Sessao sessao = new Sessao(pautaEntity);
+    	sessao.setInicioVotacao(new Date());
+    	sessao.setFimVotacao(new Date(sessao.getInicioVotacao().getTime() + 3600));
+    	Date data = new Date();
+    	Voto voto = new Voto(sessao, associado, false, data);
+    	
+		Mockito.when(associadoRepository.findById(2L)).thenReturn(Optional.of(associado));
+    	Mockito.when(pautaRepository.findById(10L)).thenReturn(Optional.of(pautaEntity));
+    	Mockito.when(sessaoRepository.findByPautaId(10L)).thenReturn(sessao);
+    	Mockito.when(votoRepository.save(any())).thenReturn(voto);
+    	
+    	VotoResponseDTO response = service.insertVoto(new VotoRequestDTO(10L, 2L, "não"));
+    	
+    	Assert.assertEquals(pautaEntity.getId(), response.getIdPauta());
+    	Assert.assertEquals(associado.getId(), response.getIdAssociado());
+    	Assert.assertEquals(sessao.getId(), response.getIdSessao());
+    	Assert.assertEquals("NÃO", response.getVoto());
+    	Assert.assertEquals(data.getTime(), response.getDataVoto().getTime());
+    }
+    
+    @Test
+    public void testVotoSemAssociado(){    	
+		Mockito.when(associadoRepository.findById(2L)).thenReturn(Optional.ofNullable(null));
+    	
+    	Assertions.assertThrows(ResourceNotFoundException.class, () -> service.insertVoto(new VotoRequestDTO(10L, 2L, "não")));
+    }
+    
+    @Test
+    public void testVotoSemPauta(){
+    	Associado associado = new Associado(2L, "Fulano", "36599334067");
+    	
+		Mockito.when(associadoRepository.findById(2L)).thenReturn(Optional.of(associado));
+    	Mockito.when(pautaRepository.findById(10L)).thenReturn(Optional.ofNullable(null));
+    	
+    	Assertions.assertThrows(ResourceNotFoundException.class, () -> service.insertVoto(new VotoRequestDTO(10L, 2L, "não")));
+    }
+    
+    @Test
+    public void testVotoSemSessao(){
+    	Associado associado = new Associado(2L, "Fulano", "36599334067");
+    	Pauta pautaEntity = new Pauta(10L, "pauta", "descricao da pauta");
+    	
+		Mockito.when(associadoRepository.findById(2L)).thenReturn(Optional.of(associado));
+    	Mockito.when(pautaRepository.findById(10L)).thenReturn(Optional.of(pautaEntity));
+    	Mockito.when(sessaoRepository.findByPautaId(10L)).thenReturn(null);
+    	
+    	Assertions.assertThrows(ConflictException.class, () -> service.insertVoto(new VotoRequestDTO(10L, 2L, "não")));
+    }
+
+    @Test
+    public void testVotoForaDoPeriodo(){
+    	Associado associado = new Associado(2L, "Fulano", "36599334067");
+    	Pauta pautaEntity = new Pauta(10L, "pauta", "descricao da pauta");
+    	Sessao sessao = new Sessao(pautaEntity);
+    	Date data = new Date();
+    	sessao.setInicioVotacao(new Date(data.getTime() - 3600));
+    	sessao.setFimVotacao(new Date(data.getTime() - 3000));
+    	Voto voto = new Voto(sessao, associado, false, data);
+    	
+		Mockito.when(associadoRepository.findById(2L)).thenReturn(Optional.of(associado));
+    	Mockito.when(pautaRepository.findById(10L)).thenReturn(Optional.of(pautaEntity));
+    	Mockito.when(sessaoRepository.findByPautaId(10L)).thenReturn(sessao);
+    	Mockito.when(votoRepository.save(any())).thenReturn(voto);
+    	
+    	Assertions.assertThrows(ForbiddenException.class, () -> service.insertVoto(new VotoRequestDTO(10L, 2L, "não")));
+    }
+
+    @Test
+    public void testVotoJaVotado(){
+    	Associado associado = new Associado(2L, "Fulano", "36599334067");
+    	Pauta pautaEntity = new Pauta(10L, "pauta", "descricao da pauta");
+    	Sessao sessao = new Sessao(pautaEntity);
+    	sessao.setInicioVotacao(new Date());
+    	sessao.setFimVotacao(new Date(sessao.getInicioVotacao().getTime() + 3600));
+    	Date data = new Date();
+    	Voto voto = new Voto(sessao, associado, false, data);
+    	
+		Mockito.when(associadoRepository.findById(2L)).thenReturn(Optional.of(associado));
+    	Mockito.when(pautaRepository.findById(10L)).thenReturn(Optional.of(pautaEntity));
+    	Mockito.when(sessaoRepository.findByPautaId(10L)).thenReturn(sessao);
+    	Mockito.when(votoRepository.save(any())).thenThrow(DataIntegrityViolationException.class);
+    	
+    	Assertions.assertThrows(ConflictException.class, () -> service.insertVoto(new VotoRequestDTO(10L, 2L, "não")));
     }
 }
